77c385f7821b09e3b1030dc71308d5ba68f5407e
Darren Fung <darren@tunezy.com>
2012-08-14 16:23:47 -0300

    Update lib/youtube_it/model/subscription.rb

--- lib/youtube_it/model/subscription.rb
+++ lib/youtube_it/model/subscription.rb
@@ -1,7 +1,7 @@
 class YouTubeIt
   module Model
     class Subscription < YouTubeIt::Record
-      attr_reader :id, :title, :published, :user_id
+      attr_reader :id, :title, :published, :user_id, :raw
     end
   end
 end
