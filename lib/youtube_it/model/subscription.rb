51cbe76bb4cfdbf1bb6c5f83e5d427a27a3273d5
Darren Fung <darren@tunezy.com>
2012-08-20 10:40:54 -0400

    Adds user_id to subscription

--- lib/youtube_it/model/subscription.rb
+++ lib/youtube_it/model/subscription.rb
@@ -2,6 +2,10 @@ class YouTubeIt
   module Model
     class Subscription < YouTubeIt::Record
       attr_reader :id, :title, :published, :channel_id, :raw
+      
+      def user_id
+        self.channel_id.match('(?<=UC).+')[0] rescue nil
+      end
     end
   end
 end
